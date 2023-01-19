a2e62a29de92db5f12b6b6dfb490a8ffed521511
shaikmoeed <shaikmoeed@gmail.com>
2023-01-20 00:26:42 +0530

    Added doc string for predict_handler
    Signed-off-by: shaikmoeed <shaikmoeed@gmail.com>

--- {{cookiecutter.project_slug}}/src/api/main.py
+++ {{cookiecutter.project_slug}}/src/api/main.py
@@ -1,3 +1,5 @@
+from typing import Dict, List
+
 import mlflow
 import mlflow.sklearn
 from fastapi import FastAPI
@@ -7,8 +9,16 @@ app = FastAPI()
 
 
 @app.get("/predict")
-def predict_handler(features: list):
+def predict_handler(features: List[float]) -> Dict[str, float]:
+    """
+    This endpoint takes a list of features as input and returns a prediction.
+
+    :param features: A list of float values representing the features used for prediction.
+    :type features: List[float]
+    :return: A dictionary containing the prediction result.
+    :rtype: Dict[str, float]
+    """
     with mlflow.start_run():
         model_uri = mlflow.get_artifact_uri("model")
         model = predict.load_model(model_uri)
-        return {"prediction": predict.predict(model, [features])[0]}
+    return {"prediction": predict.predict(model, [features])[0]}
